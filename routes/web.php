6862fe34dd4ed4887e707895eb3cb6e8b6c08b3b
Shabbar Abbas <130763637+shabbr@users.noreply.github.com>
2024-09-08 08:27:12 -0700

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Dashboard\BlogController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Route::post('/test',[AuthenticatedSessionController::class, 'store'])->name('test');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::controller(BlogController::class)->group(function () {
+    Route::get('/index', 'index')->name('blog');
+    Route::get('/show/{id}', 'show')->name('blogDetails');
+
+    // Route::post('/orders', 'store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
