abada4fcb9414b981a01051f996104549e3c2760
Hirakhizar <hirakhizarkhizarhayat@gmail.com>
2024-09-18 23:27:16 -0700

    delete service and its relevents

--- routes/api.php
+++ routes/api.php
@@ -18,19 +18,7 @@ Route::prefix('blogs')->group(function () {
     Route::get('/{id}', [BlogAPiController::class, 'show']);
     Route::get('/', [BlogAPiController::class, 'index']);
 });
- Route::prefix('services')->group(function () {
-    Route::get('/{id}', [ServiceApiController::class, 'show']);
-    Route::get('/', [ServiceApiController::class, 'index']);
 
- });
- Route::prefix('service-key-points')->group(function (){
-    Route::get('/', [ServiceKeyPointsApiController::class, 'index']);
-Route::get('/{id}', [ServiceKeyPointsApiController::class, 'show']);
-});
-Route::get('/projects',[ServiceApiController::class,'getProjects']);
- Route::prefix('service-detail')->group(function (){
-    Route::get('/{id}', [ServiceDetailApiController::class, 'show']);
-    Route::get('/', [ServiceDetailApiController::class, 'index']);
-    
-});
+
+
  
